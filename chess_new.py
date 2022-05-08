b281c7c52683d96c9d8e900165bcc2dd51f0472b
syalosovetskyi <43996021+syalosovetskyi@users.noreply.github.com>
2022-05-08 23:56:53 +0300

    fix

--- chess_new.py
+++ chess_new.py
@@ -1,16 +1,18 @@
-from collections import namedtuple
+import copy
 import pprint
-from termios import TAB0
+import time
+from collections import namedtuple
+# from termios import TAB0
 from time import time
 from uuid import uuid4
-import copy
-import time
+
 from pyparsing import col
 
 
-
-A_ORD = 97  #ord of 'a'
+# ord of 'a'
+A_ORD = 97
 pp = pprint.PrettyPrinter()
+
 
 def get_sign_zero(num):
     if num >= 1:
@@ -30,6 +32,7 @@ def get_sign(x,y):
 class V:
     x = 0
     y = 0
+
     def __init__(self, x, y=-1):
         if isinstance(x, str):
             if y == -1:
@@ -40,51 +43,63 @@ class V:
         else:
             self.x = x
             self.y = y
+
     def __repr__(self) -> str:
         return f'V({self.x},{self.y})'
+
     def __eq__(self, v):
-        if (isinstance(v, V) and 
-            self.x == v.x and self.y == v.y):
+        if (isinstance(v, V) and
+                self.x == v.x and self.y == v.y):
             return True
         else:
             return False
+
     def __add__(self, v):
         if isinstance(v, V):
             return V(self.x + v.x, self.y + v.y)
         else:
             return V(self.x + v, self.y + v)
+
     def __sub__(self, v):
         if isinstance(v, V):
             return V(self.x - v.x, self.y - v.y)
         else:
             return V(self.x - v, self.y - v)
+
     def __mul__(self, v):
         if isinstance(v, V):
             return V(self.x * v.x, self.y * v.y)
         else:
             return V(self.x * v, self.y * v)
+
     def to_print(self):
-        abc = {i:chr(i+A_ORD) for i in range(8)}
+        abc = {i: chr(i+A_ORD) for i in range(8)}
         try:
             _s = f'{abc[self.y]}{8 - self.x}'
         except Exception:
             _s = f'error {self.y} {self.x}'
         return _s
+
     def print(self):
         print(self.to_print())
 
+
 class Move():
-    def __init__(self, from_v:V, to_v:V):
+
+    def __init__(self, from_v: V, to_v: V):
         self.from_v = from_v
         self.to_v = to_v
+
     def __repr__(self) -> str:
         return f'Move({self.from_v},{self.to_v})'
+
     def __eq__(self, move):
-        if (isinstance(move, Move) and 
-            self.from_v == move.from_v and self.to_v == move.to_v):
+        if (isinstance(move, Move) and
+                self.from_v == move.from_v and self.to_v == move.to_v):
             return True
         else:
             return False
+
     def print(self):
         print(repr(self))
 
@@ -93,18 +108,21 @@ class CurrentColor:
     def __init__(self, colors):
         self.colors = colors
         self.current_color = colors[0]
+
     def __repr__(self) -> str:
         return f'{self.current_color}'
+
     def switch(self):
         for color in self.colors:
             if color == self.current_color:
                 self.current_color = self.colors[
                         (self.colors.index(color) + 1) % len(self.colors)
-                    ]
+                                                    ]
                 return self.current_color
 
 
-def get_cells_between(p1:V, p2:V):
+def get_cells_between(p1: V, p2: V):
+    cells = []
     if p1.x == p2.x:
         cells = [V(p1.x, i) for i in range(min(p1.y, p2.y)+1, max(p1.y, p2.y))]
     if p1.y == p2.y:
@@ -116,11 +134,12 @@ def get_cells_between(p1:V, p2:V):
     return cells
 
 
-def safe_divide(a,b):
+def safe_divide(a, b):
     if b == 0:
         return 0
     else:
         return a/b
+
 
 def _in(value, limit_min, limit_max):
     if value > limit_min and value < limit_max:
@@ -135,6 +154,7 @@ def _out(value, limit_min, limit_max):
     else:
         return False
 
+
 def _not(value, *args):
     for arg in args:
         if value == arg:
@@ -142,13 +162,10 @@ def _not(value, *args):
     return True
 
 
-
-
-
-
-Figures = namedtuple('Figures' , 'type color')
-Fig_State = namedtuple('Fig_State' , 'type color position')
-Fig_Rules = namedtuple('Fig_Rules' , 'moves ghost first_move move_fight step score type transform k')
+Figures = namedtuple('Figures', 'type color')
+Fig_State = namedtuple('Fig_State', 'type color position')
+Time_Counter = namedtuple('Time_Counter', 'start_time max_time')
+Fig_Rules = namedtuple('Fig_Rules', 'moves ghost first_move move_fight step score type transform k')
 
 
 class RuleSet:
@@ -210,8 +227,8 @@ class RuleSet:
         return False
 
 
-    def __init__(self):
-
+    def __init__(self, _print=0):
+        self._print = _print
         self.width = 8
         self.height = 8
         self.types = {
@@ -455,14 +472,16 @@ class RuleSet:
         figure = figures[figure_id]
         new_figure_id = board[position.x][position.y]
         if new_figure_id == 0:
-            print('if_can_move_or_beat', figure_id, figure, new_figure_id, position)
+            if self._print > 2:
+                print('if_can_move_or_beat', figure_id, figure, new_figure_id, position)
 
             return 'move'
         else:
             new_figure = figures[new_figure_id]
             if new_figure.color != figure.color:
-                print('if_can_move_or_beat', new_figure.color, new_figure.type, new_figure_id, position)
-                print('if_can_move_or_beat', figure.color, figure.type, figure_id)
+                if self._print > 2:
+                    print('if_can_move_or_beat', new_figure.color, new_figure.type, new_figure_id, position)
+                    print('if_can_move_or_beat', figure.color, figure.type, figure_id)
                 return 'beat'
         return False
 
@@ -523,7 +542,8 @@ class RuleSet:
                     _from = self.get_position_by_id(_from)
                 elif not isinstance(_to, V):
                     _to = self.get_position_by_id(_to)
-            print(board[_to.x][_to.y])
+            if self._print > 2:
+                print(board[_to.x][_to.y])
             if board[_to.x][_to.y] == 0 or board[_from.x][_from.y] == 0:
                 board[_from.x][_from.y], board[_to.x][_to.y] = \
                     board[_to.x][_to.y], board[_from.x][_from.y]
@@ -548,7 +568,7 @@ class RuleSet:
                 pass
 
 
-    def get_available_steps(self, figure_id:int, figures:dict, _board:list, _print=0):
+    def get_available_steps(self, figure_id:int, figures:dict, _board:list):
         board = copy.deepcopy(_board)
         figure = figures[figure_id]
         position = self.get_position_by_id(figure_id, board=board)
@@ -568,16 +588,17 @@ class RuleSet:
             # режим действия, если на первом ходу фигуры действуют особые условия
         else:
             moves_list.append('move') 
-        print('move3')
-        if _print > 2:
+        if self._print > 2:
             self.prettify(board, true_look=True)
             self.prettify(board)
         if figure_rule.moves.get('fight'):
             moves_list.append('fight')
         for moves in moves_list:
-            print(moves)
+            if self._print > 1:
+                print(moves)
             for move in figure_rule.moves[moves]:
-                print(moves, move)
+                if self._print > 1:
+                    print(moves, move)
                 if figure.color == 'white':
                     move = move * -1
                 if figure_rule.type == 'fast' or figure_rule.type == moves:
@@ -598,30 +619,33 @@ class RuleSet:
                         if moves == 'fight':
                             res = self.if_can_beat(figure_id, new_position, board)
                             enemy_id = self.get_id_by_pos(new_position, board)
-                            print('can beat', res, new_position, enemy_id)
+                            if self._print > 1:
+                                print('can beat', res, new_position, enemy_id)
                             
 
                         elif figure_rule.move_fight:
                             res = self.if_can_move_or_beat(figure_id, new_position, board)
                             enemy_id = self.get_id_by_pos(new_position, board)
-                            print('can move or beat', res, new_position, enemy_id)
+                            if self._print > 1:
+                                print('can move or beat', res, new_position, enemy_id)
                         else:
                             res = self.if_can_move(new_position, board)
-                            print('can move')
+                            if self._print > 1:
+                                print('can move')
                         if res:
-                            if _print > 1:
+                            if self._print > 1:
                                 self.move_figure(position, new_position, board=board)
-                            if _print > 6:
+                            if self._print > 6:
                                 pp.pprint(board)
                                 self.prettify(board)
                                 self.prettify(board, true_look=True)
-                            if _print > 1:
+                            if self._print > 1:
                                 self.prettify(board)
                                 self.move_figure(new_position, position, board=board)
                             available_moves['to'].append(new_position)
                             if res == 'beat':
-                                
-                                print(f'beating {enemy_id} on {new_position}')
+                                if self._print > 1:
+                                    print(f'beating {enemy_id} on {new_position}')
                                 score = self.rules[figures[enemy_id].type].score
                                 available_moves['score'][new_position.to_print()] = score
                         else:
@@ -733,16 +757,16 @@ class Tree():
                 print(e)
                 break
         return res
-           
 
 
 class Game():
     current_color = ''
+
     def __init__(self, color):
         self.current_color = color
         self.board = RuleSet().board
         self.figures = RuleSet().fig_state
-    
+
     def prettify(self):
         return RuleSet().prettify(_board=self.board)
 
@@ -752,21 +776,15 @@ class Game():
             color = self.current_color
         for fig in self.figures:
             if self.figures[fig].color == color or color == 'all':
-                figures[fig] = self.figures[fig]  
+                figures[fig] = self.figures[fig]
         return figures
 
 
-
-ruleset = RuleSet()
-
 current_color = 'white'
-
 
 game = Game('white')
 game.get_all_figures('black')
 game.prettify()
-
-
 
 figure = 2
 
@@ -776,7 +794,7 @@ moves = {}
 figures = game.get_all_figures('all')
 figures = game.get_all_figures(colors[0])
 
-for figure_id, figure  in figures.items():
+for figure_id, figure in figures.items():
     print(figure_id, figure)
     moves[figure_id] = ruleset.get_available_steps(figure_id, figures, game.board)
 pp.pprint(moves)
@@ -827,9 +845,10 @@ for figure_id, figure in figures.items():
 moves
 
 
-game = Game('white')
-import networkx as nx
+
 import matplotlib.pyplot as plt
+import networkx as nx
+
 
 # Defining a Class
 class GraphVisualization:
@@ -895,118 +914,110 @@ board = copy.deepcopy(game.board)
 # cc.current_color
 
 
-def start_game(game, color: CurrentColor, max_time):
+def start_game(color: CurrentColor, max_time, _print=0):
     # figures: list[Fig_State] = game.get_all_figures(color.current_color)
+    Tree().reinit()
+    max_time *= 20
     tree = Tree()
+    ruleset = RuleSet(_print=_print)
+    game = Game('white')
     trees = {}
     board = copy.deepcopy(game.board)
+    figures: list[Fig_State] = copy.deepcopy(game.get_all_figures('all'))
 
     start_time = time.time()
-    _time = {'start_time': start_time, 'end_time': max_time}
-    _trees = next_round(board, color, tree, _time)
+    _time = Time_Counter(start_time=start_time, max_time=max_time)
+    _trees = next_round(ruleset, board, figures, color, tree, _time, _print=_print)
     trees.update(_trees)
-    print(f'time is spend: {time.time() - _time["start_time"]}')
+    print(f'time is spend: {time.time() - _time.start_time}')
 
-    # board = copy.deepcopy(game.board)
-    # trees = {}
-    # moves = {}
-    # for figure_id, figure in figures.items():
-    #     print('calc available moves ', figure_id, figure)
-    #     moves[figure_id] = ruleset.get_available_steps(figure_id,
-    #                                                    figures,
-    #                                                    board)
-
-    # for fig_id, move in moves.items():
-    #     if move['to']:
-    #         for move_to in move['to']:
-    #             print('available moves ', fig_id, move['from'], move_to)
-
-    #             board = ruleset.move_figure(move['from'], move_to, board)
-
-    #             ruleset.prettify(board)
-
-    #             old_position = figures[fig_id].position
-
-    #             tree = Tree(t)
-    #             trees[tree.id] = {'fig_id': fig_id,
-    #                               'move': move,
-    #                               'figure': figures[fig_id]}
-
-    #             figures[fig_id] = Fig_State(figures[fig_id].type,
-    #                                         figures[fig_id].color,
-    #                                         move_to)
-
-    #             color.switch()
-    #             _trees = next_round(board, color, tree)
-    #             trees.update(_trees)
-
-    #             figures[fig_id] = Fig_State(figures[fig_id].type,
-    #                                         figures[fig_id].color,
-    #                                         old_position)
-
-    #             board = ruleset.move_figure(move_to, move['from'], board)
     return trees, tree
 
 
-def next_round(board, color, tree, _time):
-    _board = copy.deepcopy(board)
-    figures: list[Fig_State] = game.get_all_figures(color.current_color)
-    moves = {}
-    trees = {}
+def next_round(ruleset, board, figures, color, tree, _time: Time_Counter, _print=0, message=''):
 
-    for figure_id, figure in figures.items():
-        print(figure_id, figure)
-        moves[figure_id] = ruleset.get_available_steps(figure_id,
-                                                        figures,
-                                                        _board)
+    if (time.time() - _time.start_time) > _time.max_time:
+        if _print > 0:
+            ruleset.prettify(board)
 
-    if (time.time() - _time['start_time']) > _time['end_time']:
         return trees
     else:
-        print(f'time is spend: {time.time() - _time["start_time"]}')
+        if _print > 1:
+            print(f'time is spend: {time.time() - _time.start_time}')
+
+    _board = copy.deepcopy(board)
+    _figures: list[Fig_State] = copy.deepcopy(figures)
+    moves = {}
+    trees = {}
+    moves_len = 0
+    
+    for figure_id, figure in _figures.items():
+        # print(figure_id, figure.color, color.current_color)
+        if figure.color == color.current_color:
+            # print(figure_id, figure)
+            moves[figure_id] = ruleset.get_available_steps(figure_id, _figures, _board)
+            moves_len += len(moves[figure_id]['to'])
+    try:
+        time_2_move = _time.max_time/moves_len
+    except ZeroDivisionError:
+        time_2_move = _time.max_time
+
+    print(f'moves_len: {moves_len} time_2_move: {time_2_move} spend time {time.time() - _time.start_time}')
+
     for fig_id, move in moves.items():
         if move['to']:
             for move_to in move['to']:
-                print('available moves ', fig_id, move['from'], move_to)
+                if _print > 1:
+                    print('available moves ', fig_id, move['from'], move_to)
 
                 _board = ruleset.move_figure(move['from'], move_to, board=_board)
 
-                old_position = figures[fig_id].position
+                # old_position = _figures[fig_id].position
 
                 _tree = Tree(tree)
                 trees[_tree.id] = {'fig_id': fig_id,
                                    'move': move,
-                                   'figure': figures[fig_id]}
+                                   'figure': _figures[fig_id]}
 
-                figures[fig_id] = Fig_State(figures[fig_id].type,
-                                            figures[fig_id].color,
+                _figures[fig_id] = Fig_State(_figures[fig_id].type,
+                                            _figures[fig_id].color,
                                             move_to)
-
-                ruleset.prettify(_board)
+                if _print > 2:
+                    ruleset.prettify(_board)
 
                 color.switch()
-
-                if (time.time() - _time['start_time']) < _time['end_time']:
-                    _trees = next_round(board, color, tree, _time)
+                _message = message + f'moves from {color.current_color}   {fig_id} {move["from"]} {move_to}\n'
+                if (time.time() - _time.start_time) < time_2_move:
+                    __time = Time_Counter(start_time=time.time(), max_time=time_2_move)
+                    _trees = next_round(ruleset, board, _figures, color, _tree, __time, _print=_print, message=_message)
                 else:
-                    print('time is out')
+                    if _print > 0:
+                        ruleset.prettify(_board)
+                        print(message)
+                    if _print > 1:
+                        print('time is out')
                     _trees = {}
 
                 trees.update(_trees)
 
-                figures[fig_id] = Fig_State(figures[fig_id].type,
-                                            figures[fig_id].color,
-                                            old_position)
+                # figures[fig_id] = Fig_State(figures[fig_id].type,
+                #                             figures[fig_id].color,
+                #                             old_position)
                 _board = ruleset.move_figure(move_to, move['from'], _board)
-                print('\n\n\n')
+                if _print > 1:
+                    print('\n\n\n')
     return trees
 
 
+# figures: list[Fig_State] = copy.deepcopy(game.get_all_figures('all'))
+
 cc.switch()
 print('current color', cc.current_color)
-trees, t = start_game(game, cc, 1)
-t.childs[639]
-t.childs[2]
+trees, t = start_game(cc, 2, _print=1)
+len(t.childs)
+t.childs[0]
+t.childs[1].childs[1]
+t.childs[1].childs[1].childs[2]
 
 for _t in t.childs:
     
@@ -1026,15 +1037,4 @@ _board = copy.deepcopy(board)
 
 ruleset.prettify(_board)
 
-orig = {
-   'A': 1,
-   'B': 2,
-   'C': 3,
-}
 
-extra = {
-   'D': 4,
-   'E': 5,
-}
-
-orig.update(extra)
